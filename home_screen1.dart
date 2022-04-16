b44c93f30777b115187c030655232a09c3426653
Varun Verma <56150770+varunverma760@users.noreply.github.com>
2022-04-17 02:14:13 +0530

    Update home_screen1.dart

--- home_screen1.dart
+++ home_screen1.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter/rendering.dart';
+// import 'package:flutter/rendering.dart';
 
 class HomeScreen extends StatefulWidget {
   const HomeScreen({Key? key}) : super(key: key);
@@ -29,9 +29,42 @@ class _HomeScreenState extends State<HomeScreen> {
 
                 decoration: BoxDecoration(color: Colors.black),
               ),
+              AppBar(
+                backgroundColor: Colors.black,
+                  actions: <Widget>[
+                    Container(
+                      height: 74,
+                     child: Row(
+
+                       crossAxisAlignment: CrossAxisAlignment.end,
+                       children: <Widget>[
+                       CircleAvatar(
+                       radius: 32,
+                       backgroundColor: Colors.white,
+                     ),
+                         SizedBox(
+                           width: 16,
+                         ),
+                         Column(
+                           mainAxisAlignment: MainAxisAlignment.center,
+                           crossAxisAlignment: CrossAxisAlignment.end,
+                             children: <Widget>[
+
+
+                             ]
+                         ),
+
+
+                    ] ),),
+
+
+
+
+                  ],
+              ),
               SafeArea(
                 child: Padding(
-                  padding: EdgeInsets.all(16.0),
+                  padding: EdgeInsets.only(left: 20.0),
                   child: Column(
                     children: <Widget>[
                       Container(
@@ -46,6 +79,7 @@ class _HomeScreenState extends State<HomeScreen> {
 
 
 
+
                             SizedBox(
                               width: 16,
                             ),
@@ -53,15 +87,12 @@ class _HomeScreenState extends State<HomeScreen> {
                               mainAxisAlignment: MainAxisAlignment.center,
                               crossAxisAlignment: CrossAxisAlignment.start,
                               children: <Widget>[
-                                Text(
-                                  "My Profile",
-                                  style:
-                                  TextStyle(color: Colors.white, fontSize: 20),
-                                ),
+
                               ],
                             ),
                           ],
                         ),
+
                       ),
 
                       Column( children:<Widget>[
@@ -69,94 +100,126 @@ class _HomeScreenState extends State<HomeScreen> {
                             fontSize: 35,
                             color: Colors.white
                         )),
-                        Text('Empowering financial Independence',style:TextStyle(
+                        Text('Empowering Financial Independence',style:TextStyle(
                             fontSize: 18,
                             color: Colors.grey
-                        ))
+                        ),),
+                        Text('Welcome User',style: TextStyle(
+                            fontSize: 18,
+                            color: Colors.white
+                        ),)
                       ]
                       ),
                       Expanded(
                         child: ListView(
                           children: <Widget>[
                             GridView.count(
+                              childAspectRatio: 4/3,
                               crossAxisCount: 2,
                               physics: NeverScrollableScrollPhysics(), // to disable GridView's scrolling
                               shrinkWrap: true, // You won't see infinite size error
                               children: <Widget>[
-                                Card(
-                                  shape: RoundedRectangleBorder(
-                                      borderRadius: BorderRadius.circular(12)),
-                                  elevation: 2,
-                                  child:Align(alignment: Alignment.bottomCenter,
-                                    child:Text("   Upcoming IPOs    ",style: TextStyle(fontSize: 18,color: Colors.black,fontWeight: FontWeight.bold),
-                                    ),),
-                                ),
+                                SizedBox(
+                                  width: 173,
+                                  height: 282,
+                                  child: Card(
+                                      margin: EdgeInsets.symmetric(vertical: 10, horizontal: 20),
+                                    shape: RoundedRectangleBorder(
+                                        borderRadius: BorderRadius.circular(12)),
+                                    elevation: 12,
+                                    child:Align(alignment: Alignment.bottomCenter,
+                                      child:Text("Upcoming IPOs",style: TextStyle(fontSize: 18,color: Colors.black,fontWeight: FontWeight.bold),
+                                      ),),
+                                  ),),
 
 
 
-                                Card(
-                                  shape: RoundedRectangleBorder(
-                                      borderRadius: BorderRadius.circular(12)),
-                                  elevation: 4,
 
 
-                                  child:Align(alignment: Alignment.bottomCenter,
-                                    child:Text("   Reports    ",style: TextStyle(fontSize: 18,color: Colors.black,fontWeight: FontWeight.bold),
-                                    ),),
-                                ),
+                                SizedBox(
+                                  width: 173,
+                                  height: 282,
+                                  child: Card(
+                                      margin: EdgeInsets.symmetric(vertical: 10, horizontal: 20),
+                                    shape: RoundedRectangleBorder(
+                                        borderRadius: BorderRadius.circular(12)),
+                                    elevation: 12,
+                                    child:Align(alignment: Alignment.bottomCenter,
+                                      child:Text("Reports",style: TextStyle(fontSize: 18,color: Colors.black,fontWeight: FontWeight.bold),
+                                      ),),
+                                  ),),
                               ],
                             ),
                             GridView.count(
+                              childAspectRatio: 4/3,
                               crossAxisCount: 2,
                               physics: NeverScrollableScrollPhysics(), // to disable GridView's scrolling
                               shrinkWrap: true, // You won't see infinite size error
                               children: <Widget>[
-                                Card(
-                                  shape: RoundedRectangleBorder(
-                                      borderRadius: BorderRadius.circular(12)),
-                                  elevation: 4,
-                                  child:Align(alignment: Alignment.bottomCenter,
-                                    child:Text("   News   ",style: TextStyle(fontSize: 18,color: Colors.black,fontWeight: FontWeight.bold),
-                                    ),),
-
-                                ),
-                                Card(
-                                  shape: RoundedRectangleBorder(
-                                      borderRadius: BorderRadius.circular(12)),
-                                  elevation: 4,
-                                  child:Align(alignment: Alignment.bottomCenter,
-                                    child:Text("   Tools  ",style: TextStyle(fontSize: 18,color: Colors.black,fontWeight: FontWeight.bold),
-                                    ),),
-                                ),
+                                SizedBox(
+                                  width: 173,
+                                  height: 282,
+                                  child: Card(
+                                      margin: EdgeInsets.symmetric(vertical: 10, horizontal: 20),
+                                    shape: RoundedRectangleBorder(
+                                        borderRadius: BorderRadius.circular(12)),
+                                    elevation: 12,
+                                    child:Align(alignment: Alignment.bottomCenter,
+                                      child:Text("News",style: TextStyle(fontSize: 18,color: Colors.black,fontWeight: FontWeight.bold),
+                                      ),),
+                                  ),),
+                                SizedBox(
+                                  width: 173,
+                                  height: 282,
+                                  child: Card(
+                                      margin: EdgeInsets.symmetric(vertical: 10, horizontal: 20),
+                                    shape: RoundedRectangleBorder(
+                                        borderRadius: BorderRadius.circular(12)),
+                                    elevation: 12,
+                                    child:Align(alignment: Alignment.bottomCenter,
+                                      child:Text("Tools",style: TextStyle(fontSize: 18,color: Colors.black,fontWeight: FontWeight.bold),
+                                      ),),
+                                  ),),
 
                               ],
                             ),
 
-                            Container(
-
-                              height: 180,
-                              width: double.infinity,
-                              color: Colors.white,
-                              alignment: Alignment.bottomCenter,
-                              margin: EdgeInsets.all(20),
-                              padding: EdgeInsets.all(30),
-                              child:Align(alignment: Alignment.bottomCenter,
-                                child:Text("   Quiz Updates  ",style: TextStyle(fontSize: 18,color: Colors.black,fontWeight: FontWeight.bold),
+                            SizedBox(
+                              width: 445,
+                              height: 162,
+                              child: Card(
+                                  margin: EdgeInsets.symmetric(vertical: 10, horizontal: 20),
+                                shape: RoundedRectangleBorder(
+                                    borderRadius: BorderRadius.circular(12)),
+                                  elevation: 12,
+                                child:Align(alignment: Alignment.bottomCenter,
+                                  child:Text("Quiz Updates",style: TextStyle(fontSize: 18,color: Colors.black,fontWeight: FontWeight.bold),
+                                  ),),
+                            ),),
+                              SizedBox(
+                                width: 445,
+                                height: 162,
+                                child: Card(
+                                    margin: EdgeInsets.symmetric(vertical: 10, horizontal: 20),
+                                  shape: RoundedRectangleBorder(
+                                      borderRadius: BorderRadius.circular(12)),
+                                  elevation: 12,
+                                  child:Align(alignment: Alignment.bottomCenter,
+                                    child:Text("   Tailored Portfolio",style: TextStyle(fontSize: 18,color: Colors.black,fontWeight: FontWeight.bold),
+                                    ),),
                                 ),),
-                            ),
-                            Container(
-
-                              height: 180,
-                              width: double.infinity,
-                              color: Colors.white,
-                              alignment: Alignment.bottomCenter,
-                              margin: EdgeInsets.all(20),
-                              padding: EdgeInsets.all(30),
-                              child:Align(alignment: Alignment.bottomCenter,
-                                child:Text("   Tailored Portfolio ",style: TextStyle(fontSize: 18,color: Colors.black,fontWeight: FontWeight.bold),
-                                ),),
-                            ),
-
+                            SizedBox(
+                              width: 445,
+                              height: 162,
+                              child: Card(
+                                margin: EdgeInsets.symmetric(vertical: 10, horizontal: 20),
+                                shape: RoundedRectangleBorder(
+                                    borderRadius: BorderRadius.circular(12)),
+                                elevation: 12,
+                                child:Align(alignment: Alignment.bottomCenter,
+                                  child:Text("   Tailored Portfolio",style: TextStyle(fontSize: 18,color: Colors.black,fontWeight: FontWeight.bold),
+                                  ),),
+                              ),
 
 
 
@@ -168,24 +231,18 @@ class _HomeScreenState extends State<HomeScreen> {
 
 
 
-                         ],),
 
 
 
 
 
-
-
-
-                    ),
-                  ],),
-                ),
-              ),
-
-            ],
-
-          ),
-        )
+    ),
+    ]
+                        ),
+    ),]
+    ),),
+    ),],
+    ),)
     );
 
   }
